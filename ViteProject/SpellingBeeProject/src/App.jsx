e34edb60a48d8489b5e58e72543444ef04409c8b
ntaneja1985 <93190220+ntaneja1985@users.noreply.github.com>
2024-10-16 10:21:51 +0530

    Finished off the Spelling Bee Project
    Finished off the Spelling Bee Project

--- ViteProject/SpellingBeeProject/src/App.jsx
+++ ViteProject/SpellingBeeProject/src/App.jsx
@@ -2,25 +2,38 @@ import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { Honeycomb } from "./components/Honeycomb";
 import { Guess } from "./components/Guess";
+import { Score } from "./components/Score";
+import { CorrectGuesses } from "./components/CorrectGuesses";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState();
   const [guess, setGuess] = useState("");
+  const [correctGuesses,setCorrectGuesses] = useState([]);
   const addLetter = (letter) => {
     setGuess((g) => g + letter);
   };
+
+  const addCorrectGuesses = (guess) => {
+    setCorrectGuesses([...correctGuesses,guess]);
+  }
 
   const removeLetter = () => {
     //Remove the final character from the string
     setGuess(guess.slice(0, -1));
   };
   const checkGuess = () => {
-    if (data.answers && data.answers.includes(guess)) {
+    if(correctGuesses.includes(guess))
+    {
+      console.log("Already Found");
+    } else if (data.answers && data.answers.includes(guess)) {
+      addCorrectGuesses(guess);
       console.log("Good Job");
     } else {
       console.log("Not in the list");
     }
+
+    setGuess('');
   };
 
 
@@ -43,10 +56,13 @@ function App() {
       {data ? (
         <>
           <Header date={data.displayDate} editor={data.editor} />
+          <Score correctGuesses = {correctGuesses} />
+          <CorrectGuesses correctGuesses={correctGuesses}/>
           <section className="container">
             <div className="inputs">
               <div className="center">
-                <Guess guess = {guess} />
+                <Guess guess = {guess}  centerLetter={data.centerLetter}
+                  outerLetters={data.outerLetters} />
                 <Honeycomb
                   centerLetter={data.centerLetter}
                   outerLetters={data.outerLetters}
--- ViteProject/SpellingBeeProject/src/components/Guess.jsx
+++ ViteProject/SpellingBeeProject/src/components/Guess.jsx
@@ -1,10 +1,14 @@
-export function Guess({guess})
+export function Guess({guess,centerLetter,outerLetters})
 {
     return (
         <>
         <section className="guess">
             <p className="guess-letters">
-                {guess}
+                {guess.split('').map((l,index)=>{
+                    return <b key={index} className={"guess-letter "+ (l ===centerLetter ?
+                        "guess-center" : outerLetters.includes(l) ? "guess-outer": null
+                    )}>{l}</b>
+                })}
             </p>
         </section>
         </>
